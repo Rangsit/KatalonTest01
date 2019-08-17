b13d58756ab39abcee950fe172eec3b066c98e6a
Rangsit <softwaretest.se1@gmail.com>
2019-08-17 14:30:59 +0700

    Update Test Suite

---
+++ Test Suites/TS-FindJob.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-FindJob</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>edbaee38-afc7-4db5-bfcb-7a8efa5b38e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b26bbb54-0371-4ae1-b439-1e7c43969ed2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-Ex_indeed</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
